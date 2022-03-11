5c7edf8577c4a9cfe7d9d6b2df0fa61089e58182
Maïmouna Bah <71721866+MaimounaBah@users.noreply.github.com>
2022-03-11 13:48:42 +0100

    End of pom.xml configuration

---
+++ src/test/java/tp10/metier/ActionTest.java
@@ -0,0 +1,19 @@
+package tp10.metier;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author 33760
+ */
+public class ActionTest {
+    
+    public ActionTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
