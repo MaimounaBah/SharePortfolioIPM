e45cfd4bf92f9e998a30efd09b01987b86ebfc0f
Maïmouna Bah <71721866+MaimounaBah@users.noreply.github.com>
2022-03-11 14:26:14 +0100

    Update licence headers

--- src/main/java/tp10/metier/NewClass.java
+++ src/main/java/tp10/metier/NewClass.java
@@ -15,20 +15,10 @@
  */
 package tp10.metier;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
 /**
  *
  * @author 33760
  */
-public class ActionTest {
-    
-    public ActionTest() {
-    }
-
-    @Test
-    public void testSomeMethod() {
-    }
+public class NewClass {
     
 }
